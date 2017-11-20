a0c7ff4d8d64c15c9d3a18dca493c42c63ed373a
Gergely Juhasz <juhaszgege@gmail.com>
2017-11-20 01:59:35 +0000

    almost there

--- vislgate_levels.js
+++ vislgate_levels.js
@@ -6,7 +6,7 @@ var vislGateLevels = {
         levelDescription: 'Mauris non nisi nec velit semper fringilla ut eu dui. Sed sagittis magna quis vestibulum scelerisque. Suspendisse a volutpat magna. Morbi fringilla scelerisque finibus. Vestibulum accumsan auctor ullamcorper. Duis dapibus nibh sit amet nulla tristique imperdiet. Suspendisse congue mi non purus blandit suscipit. Duis eget metus felis. Nulla ut suscipit est, a semper lorem. Nam ut nisl felis.',
         level: [
             {id: 1, type: 'source', x: 0, y: 50, signal: true, slotName: 'Slot - A'},
-            {id: 2, type: 'source', x: 0, y: 150, signal: true, slotName: 'Slot - B'},
+            {id: 2, type: 'source', x: 0, y: 150, signal: false, slotName: 'Slot - B'},
             {id: 3, type: 'gate', subtype: 'empty', x: 250, y: 100, slotName: 'Slot - 1'},
             {id: 4, type: 'display', x: 550, y: 100, slotName: 'Output'},
         ],
@@ -38,7 +38,7 @@ var vislGateLevels = {
         levelDescription: 'Mauris non nisi nec velit semper fringilla ut eu dui. Sed sagittis magna quis vestibulum scelerisque. Suspendisse a volutpat magna. Morbi fringilla scelerisque finibus. Vestibulum accumsan auctor ullamcorper. Duis dapibus nibh sit amet nulla tristique imperdiet. Suspendisse congue mi non purus blandit suscipit. Duis eget metus felis. Nulla ut suscipit est, a semper lorem. Nam ut nisl felis.',
         level: [
             {id: 1, type: 'source', x: 0, y: 50, signal: true, slotName: 'Slot - A'},
-            {id: 2, type: 'source', x: 0, y: 150, signal: true, slotName: 'Slot - B'},
+            {id: 2, type: 'source', x: 0, y: 150, signal: false, slotName: 'Slot - B'},
             {id: 3, type: 'source', x: 0, y: 250, signal: true, slotName: 'Slot - C'},
             {id: 4, type: 'gate', subtype: 'empty', x: 150, y: 100, slotName: 'Slot - 1'},
             {id: 5, type: 'gate', subtype: 'empty', x: 350, y: 200, slotName: 'Slot - 2'},
@@ -100,9 +100,9 @@ var vislGateLevels = {
         levelTitle: 'Notandor',
         levelDescription: 'Mauris non notandor nisi nec velit semper fringilla ut eu dui. Sed sagittis magna quis vestibulum scelerisque. Suspendisse a volutpat magna. Morbi fringilla scelerisque finibus. Vestibulum accumsan auctor ullamcorper. Duis dapibus nibh sit amet nulla tristique imperdiet. Suspendisse congue mi non purus blandit suscipit. Duis eget metus felis. Nulla ut suscipit est, a semper lorem. Nam ut nisl felis.',
         level: [
-            {id: 1, type: 'source', x: 0, y: 50, signal: true, slotName: 'Slot - A'},
+            {id: 1, type: 'source', x: 0, y: 50, signal: false, slotName: 'Slot - A'},
             {id: 2, type: 'source', x: 0, y: 150, signal: true, slotName: 'Slot - B'},
-            {id: 3, type: 'source', x: 0, y: 250, signal: true, slotName: 'Slot - C'},
+            {id: 3, type: 'source', x: 0, y: 250, signal: false, slotName: 'Slot - C'},
             {id: 4, type: 'gate', subtype: 'empty', x: 150, y: 100, slotName: 'Slot - 1'},
             {id: 5, type: 'gate', subtype: 'empty', x: 150, y: 250, slotName: 'Slot - 2'},
             {id: 6, type: 'gate', subtype: 'empty', x: 350, y: 250, slotName: 'Slot - 3'},
